4ceb73b23e2c74650a06a20887898209c246bbf1
Bittu Raj <b4bittukumar27156@gmail.com>
2026-01-16 17:20:39 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/my-blog/",
+  base: mode === 'github' ? '/my-blog/' : '/',   // 🔥 AUTO SWITCH
   build: {
-    outDir: "docs"
+    outDir: 'docs'
   }
-})
+}))
